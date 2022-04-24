4f05651fbe9ac755857a97ed23fa0000035880ef
sushinuggets <sushinuggets13@gmail.com>
2022-04-23 20:50:57 -0400

    Updated unit conversion and photo changes
    bug fix. Created new bug with geolocation function.
    
    Added feature to change background image and weather icons to match weather conditions

--- src/script.js
+++ src/script.js
@@ -45,75 +45,46 @@ dateBtn.addEventListener("click", changeDate);
 let currentDate = document.getElementById("date-time");
 currentDate.innerHTML = formatDate(new Date());
 
-// function to convert to C when radio button clicked
+
+let fahrenheitRadio = document.getElementById("fahrenheit-radio");
 let celciusRadio = document.getElementById("celcius-radio");
 
 celciusRadio.onclick = function convertCelcius() {
-  let temperature = document.getElementById("current-temp").innerHTML;
-  temperature = parseInt(temperature);
-  temperature = Math.round(((temperature - 32) * 5) / 9);
+  let units = "metric";
+  let city = document.getElementById("city-search").value;
+  city.trim();
+  let degrees = document.querySelector(".temp-unit");
+  degrees.innerHTML = "°C";
+  windUnits = "km/h";
 
-  let highTemp = document.getElementById("high-temp").innerHTML;
-  let lowTemp = document.getElementById("low-temp").innerHTML;
-  let realFeelTemp = document.getElementById("real-feel").innerHTML;
-  highTemp = parseInt(highTemp);
-  lowTemp = parseInt(lowTemp);
-  realFeelTemp = parseInt(realFeelTemp);
+  let apiKey = "77897962aee35dda8ee05a81adbd8139";
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-  highTemp = Math.round(((highTemp - 32) * 5) / 9);
-  lowTemp = Math.round(((lowTemp - 32) * 5) / 9);
-  realFeelTemp = Math.round(((realFeelTemp - 32) * 5) / 9);
-
-  let unit = document.querySelector(".temp-unit");
-  unit.innerHTML = "°C";
-  let newTemp = document.getElementById("current-temp");
-  newTemp.innerHTML = temperature;
-  let newHigh = document.getElementById("high-temp");
-  newHigh.innerHTML = `${highTemp}°`;
-  let newLow = document.getElementById("low-temp");
-  newLow.innerHTML = `${lowTemp}°`;
-  let newRealFeel = document.getElementById("real-feel");
-  newRealFeel.innerHTML = `${realFeelTemp}°`;
+  axios.get(api).then(changeWeather);
 };
-// function to convert to F when radio button clicked
-let fahrenheitRadio = document.getElementById("fahrenheit-radio");
 
 fahrenheitRadio.onclick = function convertFahreinheit() {
-  let temperature = document.getElementById("current-temp").innerHTML;
-  temperature = parseInt(temperature);
-  temperature = Math.round((temperature * 9) / 5 + 32);
+  let units = "Imperial";
+  let city = document.getElementById("city-search").value;
+  city.trim();
+  let degrees = document.querySelector(".temp-unit");
+  degrees.innerHTML = "°F";
+  windUnits = "mph";
 
-  let highTemp = document.getElementById("high-temp").innerHTML;
-  let lowTemp = document.getElementById("low-temp").innerHTML;
-  let realFeelTemp = document.getElementById("real-feel").innerHTML;
-  highTemp = parseInt(highTemp);
-  lowTemp = parseInt(lowTemp);
-  realFeelTemp = parseInt(realFeelTemp);
+  let apiKey = "77897962aee35dda8ee05a81adbd8139";
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-  highTemp = Math.round((highTemp * 9) / 5 + 32);
-  lowTemp = Math.round((lowTemp * 9) / 5 + 32);
-  realFeelTemp = Math.round((realFeelTemp * 9) / 5 + 32);
-
-  let newHigh = document.getElementById("high-temp");
-  newHigh.innerHTML = `${highTemp}°`;
-  let newLow = document.getElementById("low-temp");
-  newLow.innerHTML = `${lowTemp}°`;
-  let newRealFeel = document.getElementById("real-feel");
-  newRealFeel.innerHTML = `${realFeelTemp}°`;
-  let newTemp = document.getElementById("current-temp");
-  newTemp.innerHTML = temperature;
-  let unit = document.querySelector(".temp-unit");
-  unit.innerHTML = "°F";
+  axios.get(api).then(changeWeather);
 };
 
 // Search Engine
-
 function changeWeather(response) {
+  console.log(response);
   let cityName = response.data.name;
   let temp = Math.round(response.data.main.temp);
   let high = Math.round(response.data.main.temp_max);
   let low = Math.round(response.data.main.temp_min);
-  //let precipitation = Math.round(response.data.??);
+  let humidity = Math.round(response.data.main.humidity);
   let windSpeed = Math.round(response.data.wind.speed);
   let realFeel = Math.round(response.data.main.feels_like);
   let conditions = response.data.weather[0].description;
@@ -121,17 +92,139 @@ function changeWeather(response) {
   let newCity = document.querySelector(".city-name");
   newCity.innerHTML = cityName;
   let newTemp = document.getElementById("current-temp");
-  newTemp.innerHTML = temp;
+  newTemp.innerHTML = `${temp}`;
   let highTemp = document.getElementById("high-temp");
   highTemp.innerHTML = `${high}°`;
   let lowTemp = document.getElementById("low-temp");
   lowTemp.innerHTML = `${low}°`;
+  let newHumidity = document.getElementById("humidity");
+  newHumidity.innerHTML = `${humidity}%`;
   let newConditions = document.getElementById("weather-condition");
   newConditions.innerHTML = conditions;
-  let newWind = document.getElementById("wind-speed");
-  newWind.innerHTML = ` ${windSpeed} km/h`;
   let newReal = document.getElementById("real-feel");
   newReal.innerHTML = ` ${realFeel}°`;
+  let newWind = document.getElementById("wind-speed");
+  newWind.innerHTML = `${windSpeed} ${windUnits}`;
+
+  //change current weather icon
+  let icon = document.querySelector("#weather-icon");
+  let weatherCondition = response.data.weather[0].main.toLowerCase();
+  let description = response.data.weather[0].description;
+  let backgroundImg = document.querySelector(".background");
+  let headerText = document.querySelector(".header");
+
+  switch (weatherCondition) {
+    case "thunderstorm":
+      icon.setAttribute("src", "./weather-images/thunderstorm.png");
+      icon.setAttribute("alt", "weatherCondition");
+      backgroundImg.setAttribute(
+        "style",
+        "background-image: url('./backgrounds/thunderstorm-bg.jpg')"
+      );
+      headerText.setAttribute("style", "color: #FFFFFF");
+      break;
+      break;
+    case "drizzle":
+      icon.setAttribute("src", "./weather-images/drizzle.png");
+      backgroundImg.setAttribute(
+        "style",
+        "background-image: url('./backgrounds/rain-bg.jpg')"
+      );
+      headerText.setAttribute("style", "color: #FFFFFF");
+      break;
+    case "rain":
+      icon.setAttribute("src", "./weather-images/rain.png");
+      backgroundImg.setAttribute(
+        "style",
+        "background-image: url('./backgrounds/rain-bg.jpg');"
+      );
+      headerText.setAttribute("style", "color: #FFFFFF");
+      break;
+    case "snow":
+      icon.setAttribute("src", "./weather-images/snow.png");
+      backgroundImg.setAttribute(
+        "style",
+        "background-image: url('./backgrounds/snow-bg.jpg')"
+      );
+      headerText.setAttribute("style", "color: #000000");
+      break;
+    case "clear":
+      icon.setAttribute("src", "./weather-images/clear.png");
+      backgroundImg.setAttribute(
+        "style",
+        "background-image: url('./backgrounds/clear-bg.jpg');"
+      );
+      headerText.setAttribute("style", "color: #FFFFFF");
+      break;
+    case "mist":
+      icon.setAttribute("src", "./weather-images/mist.png");
+      backgroundImg.setAttribute(
+        "style",
+        "background-image: url('./backgrounds/mist-bg.jpg');"
+      );
+      headerText.setAttribute("style", "color: #000000");
+      break;
+    case "tornado":
+      icon.setAttribute("src", "./weather-images/tornado.png");
+      backgroundImg.setAttribute(
+        "style",
+        "background-image: url('./backgrounds/tornado-bg.jpg');"
+      );
+      headerText.setAttribute("style", "color: #FFFFFF");
+      break;
+    case "dust":
+      icon.setAttribute("src", "./weather-images/dust.png");
+      backgroundImg.setAttribute(
+        "style",
+        "background-image: url('./backgrounds/dust-bg.jpg');"
+      );
+      headerText.setAttribute("style", "color: #FFFFFF");
+      break;
+    case "sand":
+      icon.setAttribute("src", "./weather-images/dust.png");
+      backgroundImg.setAttribute(
+        "style",
+        "background-image: url('./backgrounds/dust-bg.jpg');"
+      );
+      headerText.setAttribute("style", "color: #FFFFFF");
+      break;
+    case "ash":
+      icon.setAttribute("src", "./weather-images/dust.png");
+      backgroundImg.setAttribute(
+        "style",
+        "background-image: url('./backgrounds/dust-bg.jpg');"
+      );
+      headerText.setAttribute("style", "color: #FFFFFF");
+      break;
+    case "clouds":
+      if (description === "few clouds" || description === "scattered clouds") {
+        icon.setAttribute("src", "./weather-images/few-clouds.png");
+        backgroundImg.setAttribute(
+          "style",
+          "background-image: url('./backgrounds/partly-cloudy-bg.jpg');"
+        );
+      } else if (description === "overcast clouds") {
+        icon.setAttribute("src", "./weather-images/cloudy.png");
+        backgroundImg.setAttribute(
+          "style",
+          "background-image: url('./backgrounds/cloudy-bg.jpg');"
+        );
+      } else {
+        icon.setAttribute("src", "./weather-images/partly-cloudy.png");
+        backgroundImg.setAttribute(
+          "style",
+          "background-image: url('./backgrounds/partly-cloudy-bg.jpg');"
+        );
+      }
+      break;
+    default:
+      icon.setAttribute("src", "./weather-images/mist.png");
+      backgroundImg.setAttribute(
+        "style",
+        "background-image: url('./backgrounds/mist-bg.jpg');"
+      );
+      headerText.setAttribute("style", "color: #000000");
+  }
 }
 
 let searchBtn = document.querySelector(".search-btn");
@@ -141,7 +234,6 @@ searchBtn.onclick = function searchCity(event) {
   let city = document.getElementById("city-search").value;
   city.trim();
 
-  let units;
   if (celciusRadio.checked) {
     units = "metric";
   } else {
@@ -158,7 +250,6 @@ searchBtn.onclick = function searchCity(event) {
 function locateUser(response) {
   let latitude = response.coords.latitude;
   let longitude = response.coords.longitude;
-  let units;
 
   if (celciusRadio.checked) {
     units = "metric";
@@ -175,6 +266,7 @@ function locateUser(response) {
 function startGeolocation() {
   navigator.geolocation.getCurrentPosition(locateUser);
 }
-
+let units;
+let windUnits;
 let geolocationBtn = document.getElementById("geolocation");
 geolocationBtn.addEventListener("click", startGeolocation);
